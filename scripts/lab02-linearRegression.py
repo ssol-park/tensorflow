0732250972b931dc5cb72267e1c1558bf7dcff35
ssol-park <ksttlov.naver@hanmail.net>
2024-12-04 16:40:07 +0900

    feat: GradientDescent 최적화 적용
     - Hypothesis 함수와 Cost 함수 정의
     - GradientTape를 활용한 학습 단계 구현

--- scripts/lab02-linearRegression.py
+++ scripts/lab02-linearRegression.py
@@ -7,12 +7,29 @@ y_train = tf.constant([1, 2, 3], dtype=tf.float32)
 W = tf.Variable(tf.random.normal([1]), name='weight')
 b = tf.Variable(tf.random.normal([1]), name='bias')
 
-# hypothesis(가설)
-hypothesis = x_train * W + b
+#  최적화 함수 정의
+optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
 
-print("Hypothesis: ", hypothesis)
+# 1. 가설 정의
+def hypothesis(x):
+    return  x * W + b
 
-# cost/loss function
-cost = tf.reduce_mean(tf.square(hypothesis - y_train)) # 평균 제곱 오차 (MSE)
-print("Cost: ", cost.numpy())
+# 2. cost function 정의
+def cost_function():
+    return tf.reduce_mean(tf.square(hypothesis(x_train) - y_train))
 
+# 3.학습 단계 정의 (gradient descent)
+@tf.function
+def train_step():
+    with tf.GradientTape() as tape:
+        cost = cost_function()
+    gradients = tape.gradient(cost, [W, b])           # 가중치와 바이어스에 대한 gradient 계산
+    optimizer.apply_gradients(zip(gradients, [W, b])) # 최적화 적용
+    return cost
+
+for step in range(20):
+    cost = train_step()
+    if step % 5 == 0:
+        print(f"Step {step}, Cost: {cost.numpy()}, W: {W.numpy()}, b: {b.numpy()}")
+
+print("Result: ", hypothesis(x_train).numpy())
